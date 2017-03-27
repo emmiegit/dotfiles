a8394bd09a38d2fa50d832a04357bf59e55da941
Ammon Smith <ammon.i.smith@gmail.com>
2017-03-27 14:42:19 -0700

    Fix up link.sh

--- link.sh
+++ link.sh
@@ -3,22 +3,34 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 this_dir="$(pwd -P)"
-flags=()
+flags=('-sv')
 files=(
 	'.bashrc'
 	'.vim'
 	'.vimrc'
+	'.gitconfig'
 	'.gvimrc'
 	'.zshrc'
 	'.zshrc.d'
 	'.oh-my-zsh'
 )
 
-if [[ "$1" == '-f' ]] || [[ "$1" == '--force' ]]; then
-	flags+=('-f')
-fi
+for arg in "$@"; do
+	case "$arg" in
+		-f)
+			flags+=('-f')
+			;;
+		--force)
+			flags+=('-f')
+			;;
+		*)
+			printf >&2 'Unknown argument: %s\n' "$arg"
+			exit 1
+			;;
+	esac
+done
 
 for filename in "${files[@]}"; do
-	ln -sv "${flags[@]}" "$this_dir/$filename" "$HOME" || true
+	ln "${flags[@]}" "$this_dir/$filename" "$HOME" || true
 done
 
